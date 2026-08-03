8d4ad8f909ccb0e25b9262936c98d848eb1aff32
joellebron-Analyst <joel.lebron@ualett.com>
2026-08-02 22:40:09 -0400

    Update score_web_app.py

--- score_web_app.py
+++ score_web_app.py
@@ -217,7 +217,7 @@ def conformance(name):
 
     grouped_score = score_horas_trabajadas + resultado_lunch + score_away
 
-    return math.floor(grouped_score / 3)
+    return round(grouped_score / 3, 2)
 
 
 
